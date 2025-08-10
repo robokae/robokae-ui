c5f3c2f3285e37a2e5d15740a29f1dc5829fdb85
Alexander Hom <alexanderhom19@gmail.com>
2025-08-09 18:31:20 -0700

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,73 @@
+export const gap = {
+  xs: "0.25rem",
+  sm: "0.5rem",
+  md: "1.5rem",
+  lg: "3rem",
+  xl: "5rem",
+};
+
+export const padding = {
+  xs: "0.75rem",
+  sm: "1rem",
+  md: "1.5rem",
+  lg: "2rem",
+  xl: "3rem",
+};
+
+export const layout = {
+  headerHeight: "4.5rem",
+  footerHeight: "8rem",
+  contentMaxWidth: "1200px",
+};
+
+export const breakpoint = {
+  mobileSm: "(max-width: 320px)",
+  mobileMd: "(max-width: 425px)",
+  mobileLg: "(max-width: 600px)",
+  tablet: "(max-width: 768px)",
+  laptop: "(max-width: 1024px)",
+  laptopLg: "(max-width: 1440px)",
+  desktop: "(max-width: 2560px)",
+};
+
+export const icon = {
+  xs: "0.75rem",
+  sm: "1rem",
+  md: "1.25rem",
+  lg: "1.5rem",
+  xl: "1.75rem",
+};
+
+export const theme = {
+  light: "light",
+  dark: "dark",
+};
+
+export const borderRadius = {
+  sm: "0.25rem",
+  md: "0.5rem",
+  lg: "1rem",
+  pill: "9999px",
+};
+
+export const typography = {
+  primaryFont: "Plus Jakarta Sans",
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+  fontSize: {
+    heading: {
+      h1: { sm: "2.027rem", lg: "3.185rem" },
+      h2: { sm: "1.802rem", lg: "3.052rem" },
+      h3: { sm: "1.602rem", lg: "2.441rem" },
+      h4: { sm: "1.424rem", lg: "1.953rem" },
+      h5: { sm: "1.266rem", lg: "1.563rem" },
+    },
+    body: "1rem",
+    caption: "0.875rem",
+  },
+  monospaceFont: "JetBrains Mono",
+  textShadow: "0.2rem 0.2rem 2rem rgba(0, 0, 0, 0.2)",
+};
