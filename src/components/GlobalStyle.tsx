3828d330a77de174cc71cc7c7bed497a3f58d3a6
Alexander Hom <alexanderhom19@gmail.com>
2026-02-01 14:30:52 -0800

    Add default font import to global styles

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -1,3 +1,6 @@
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/700.css";
 import { createGlobalStyle } from "styled-components";
 import { typography } from "../constants";
 
