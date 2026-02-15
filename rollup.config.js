e85e16db233a33fc0fe1fdcb15efbc1383e935af
Alexander Hom <alexanderhom19@gmail.com>
2026-02-14 17:52:26 -0800

    Update TS and Rollup configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,11 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import { dts } from "rollup-plugin-dts";
-import { createFilter } from "@rollup/pluginutils";
 import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
-
-const excludeStories = createFilter(["**/*.stories.*"]);
-
-const replaceSourceExtensions = () => ({
-  name: "replace-source-extensions",
-  renderChunk(code, chunk, options) {
-    if (chunk.map) {
-      chunk.map.sources = chunk.map.sources.map((source) =>
-        source.replace(/\.tsx$/, ".ts")
-      );
-    }
-
-    return { code, map: chunk.map };
-  },
-});
 
 export default [
   {
@@ -48,17 +32,7 @@ export default [
         useTsconfigDeclarationDir: true,
       }),
       json(),
-      replaceSourceExtensions(),
       terser(),
-      {
-        name: "exclude-stories",
-        load(id) {
-          if (typeof id === "string" && excludeStories(id)) {
-            return null;
-          }
-          return undefined;
-        },
-      },
     ],
     external: [
       ...Object.keys(packageJson.peerDependencies || {}),
